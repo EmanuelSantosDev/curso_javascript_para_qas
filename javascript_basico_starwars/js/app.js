b4583258a683ea57edb3bedfbf96c44db2ef0b8d
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-05-23 22:55:15 -0300

    Update "app.js"

--- javascript_basico_starwars/js/app.js
+++ javascript_basico_starwars/js/app.js
@@ -1,1 +1,16 @@
 console.log('Olá Javascript, seja bem-vindo!')
+
+var userName = 'Emanuel Santos'
+document.getElementById('user-name').innerHTML = userName
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true
+
+// console.log(nome)
+// console.log(idade)
+// console.log(jedi)
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
