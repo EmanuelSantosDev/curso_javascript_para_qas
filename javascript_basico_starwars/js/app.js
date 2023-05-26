54bfffae8f1dd0670890086c560069b38e131e17
Emanuel Santos <eu.emanuel.santos@outlook.com>
2023-05-25 23:22:46 -0300

    Aula "Operadores Matemáticos"

--- javascript_basico_starwars/js/app.js
+++ javascript_basico_starwars/js/app.js
@@ -3,14 +3,42 @@ console.log('Olá Javascript, seja bem-vindo!')
 var userName = 'Emanuel Santos'
 document.getElementById('user-name').innerHTML = userName
 
-// var nome = 'Mestre Yoda'
-// var idade = 100
-// var jedi = true
 
-// console.log(nome)
-// console.log(idade)
-// console.log(jedi)
+// ***********************************************
 
-// console.log(typeof nome)
-// console.log(typeof idade)
-// console.log(typeof jedi)
+// Variáveis //
+
+var nome = 'Mestre Yoda'
+var idade = 100
+var jedi = true
+
+console.log(typeof nome) // string
+console.log(typeof idade) // number
+console.log(typeof jedi) // boolean
+
+// ***********************************************
+
+// Operadores Matemáticos //
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para dividir
+
+var n1 = 5
+var n2 = 5
+var total = n1 + n2
+console.log(total) // 10
+
+var n3 = 10
+var n4 = '10'
+var total_2 = n3 + n4
+console.log(total_2) // 1010
+
+var total_3 = n3 + parseInt(n4)
+console.log(total_3) // 20
+
+var n5 = 10
+var n6 = 'dez'
+var total_4 = n5 + parseInt(n6)
+console.log(total_4) // NaN
